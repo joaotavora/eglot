92190e85bd820a62660f094789bd1a13fefe148e
John Wiegley <johnw@newartisans.com>
2002-04-08 09:54:18 +0000

    *** no comment ***

---
+++ chess-transport.el
@@ -0,0 +1,27 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; An example of a generic transport engine, based on the protocol
+;; used by chess-network.el.  The only parts missing are send and
+;; receive.  This could be used for transmitting chess.el protocol
+;; over CTCP, for example.
+;;
+;; $Revision$
+
+(require 'chess-network)
+
+(defalias 'chess-network-regexp-alist 'chess-transport-regexp-alist)
+
+(defun chess-transport-handler (event &rest args)
+  "This is an example of a generic transport engine."
+  (cond
+   ((eq event 'send)
+    ;; transmit the string given in (car args) to your outbound
+    ;; transport from here
+    )))
+
+;; call (chess-engine-submit engine STRING) for text that arrives from
+;; your inbound transport
+
+(provide 'chess-transport)
+
+;;; chess-transport.el ends here
