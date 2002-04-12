1496cc9fe0fdebe3961e1b3aac54b37529645c5b
John Wiegley <johnw@newartisans.com>
2002-04-12 22:20:36 +0000

    *** no comment ***

--- chess-engine.el
+++ chess-engine.el
@@ -258,7 +258,7 @@
 
 	(setq chess-engine-pending-offer nil
 	      chess-engine-pending-arg nil)
-	t)))))
+	t))))
 
 (defun chess-engine-create (module &optional response-handler &rest args)
   (let ((regexp-alist (intern-soft (concat (symbol-name module)
