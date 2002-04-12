1b173a311002cdd6fa0b0f0dee916815e513f1ed
John Wiegley <johnw@newartisans.com>
2002-04-12 23:30:05 +0000

    bumped minor rev

--- chess.el
+++ chess.el
@@ -82,7 +82,7 @@
   "An Emacs chess playing program."
   :group 'games)
 
-(defconst chess-version "2.0a7"
+(defconst chess-version "2.0a8"
   "The version of the Emacs chess program.")
 
 (defcustom chess-default-display (if (display-graphic-p)
