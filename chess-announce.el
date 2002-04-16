146b3661c5bfc845cd8ab6c14ee41d0bdbe98da6
Mario Lang <mlang@delysid.org>
2002-04-16 16:34:02 +0000

    german messages (no idea on how to synth yet :) )

--- chess-announce.el
+++ chess-announce.el
@@ -22,6 +22,22 @@
     (piece-moves  . "%s to %s")
     (piece-takes  . "%s takes %s at %s")))
 
+(chess-message-catalog 'german
+  '((queen	  . "Dame")
+    (king	  . "König")
+    (bishop	  . "Läufer")
+    (knight	  . "Springer")
+    (rook	  . "Turm")
+    (pawn	  . "Bauer")
+    (short-castle . "Kurze Rochade")
+    (long-castle  . "Lange Rochade")
+    (check	  . "Schach")
+    (checkmate	  . "Schach matt")
+    (stalemate	  . "Patt")
+    (en-passant	  . "on possont")
+    (piece-moves  . "%s nach %s")
+    (piece-takes  . "%s schlägt %s auf %s")))
+
 (defvar chess-announce-names
   '((?q . queen)
     (?k . king)
