b957ef47bde14987935470fece21a3e9e47d8f6f
Mario Lang <mlang@delysid.org>
2004-01-14 11:42:57 +0000

    elisp header + homepage link

--- chess-sjeng.el
+++ chess-sjeng.el
@@ -1,13 +1,33 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Play against sjeng!
-;;
+;;; chess-sjeng.el --- Play against sjeng!
+
+;; Copyright (C) 2004  Free Software Foundation, Inc.
+
+;; Author: Mario Lang <mlang@delysid.org>
+;; Keywords: games
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Code:
 
 (require 'chess-common)
 
 (defgroup chess-sjeng nil
   "The publically available chess engine 'sjeng'."
-  :group 'chess-engine)
+  :group 'chess-engine
+  :link '(url-link "http://sjeng.sourceforge.net"))
 
 (defcustom chess-sjeng-path (executable-find "sjeng")
   "*The path to the sjeng executable."
