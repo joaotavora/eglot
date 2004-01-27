e48256849bbbc9e8d0c53e8621f242dd361461b3
Mario Lang <mlang@delysid.org>
2004-01-27 21:02:50 +0000

    we can use chess-display-set-from-fen here

--- chess-tutorial.el
+++ chess-tutorial.el
@@ -33,9 +33,7 @@
 	 (display (chess-create-display t)))
     (with-current-buffer display
       (chess-module-set-leader nil)
-      (chess-game-set-start-position
-       (chess-display-game nil)
-       (chess-fen-to-pos "8/3p1p/2p3p/4q/2p3p/3p1p/8/N w - -"))
+      (chess-display-set-from-fen "8/3p1p/2p3p/4q/2p3p/3p1p/8/N w - -")
       (chess-game-add-hook (chess-display-game nil) 'chess-tutorial-knight-1)
       (setq chess-pos-always-white t)
       (chess-display-popup nil)
