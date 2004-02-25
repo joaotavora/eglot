4a8ef035b6b076c7a5d78eae8a8e4993a47544bb
Mario Lang <mlang@delysid.org>
2004-02-25 11:53:07 +0000

    add elisp header and homepage link

--- chess-phalanx.el
+++ chess-phalanx.el
@@ -1,13 +1,32 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Play against phalanx!
-;;
+;;; chess-phalanx.el --- Play chess against phalanx!
+
+;; Copyright (C) 2002, 2004  Free Software Foundation, Inc.
+
+;; Author: John Wiegley
+;; Maintainer: Mario Lang <mlang@delysid.org>
+;; Keywords: games, processes
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 
 (require 'chess-common)
 
 (defgroup chess-phalanx nil
   "The publically available chess engine 'phalanx'."
-  :group 'chess-engine)
+  :group 'chess-engine
+  :link '(url-link "http://dusan.freeshell.org/phalanx/"))
 
 (defcustom chess-phalanx-path (executable-find "phalanx")
   "The path to the phalanx executable."
