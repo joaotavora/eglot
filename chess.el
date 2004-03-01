368d79f39dd8b922cee306a260b6612605430204
Mario Lang <mlang@delysid.org>
2004-03-01 16:58:31 +0000

    bumped minor rev

--- chess.el
+++ chess.el
@@ -81,7 +81,7 @@
   "An Emacs chess playing program."
   :group 'games)
 
-(defconst chess-version "2.0b5"
+(defconst chess-version "2.0b6"
   "The version of the Emacs chess program.")
 
 (defcustom chess-default-display
