e06b864fb22acd8b455a7f722ef0f6453eb89022
Mario Lang <mlang@delysid.org>
2014-05-20 02:00:35 +0200

    Upgrade to GPLv3.

--- chess-uci.el
+++ chess-uci.el
@@ -1,28 +1,27 @@
-;;; chess-uci.el --- Universal chess interface protocol for emacs-chess
+;;; chess-uci.el --- Common functions for the Universal Chess Interface protocol
 
 ;; Copyright (C) 2014  Free Software Foundation, Inc.
 
 ;; Author: Mario Lang <mlang@delysid.org>
-;; Keywords: games
+;; Keywords: games, processes
 
-;; This file is free software; you can redistribute it and/or modify
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This file is distributed in the hope that it will be useful,
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; URL: http://en.wikipedia.org/wiki/Universal_Chess_Interface
+;; Common functions for engines based on the Universal Chess Interface.
+;; See <http://en.wikipedia.org/wiki/Universal_Chess_Interface>.
 
 ;;; Code:
 
