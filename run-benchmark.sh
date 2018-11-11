8cc883c9695a4b0c51b53e5661fbbecf53e83be2
João Távora <joaotavora@gmail.com>
2018-11-11 14:01:11 +0000

    Slightly sanitize benchmark

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,9 +1,11 @@
 set -e
 
-emacs -batch -Q -l generate-test-data.el
+EMACS=${EMACS:-emacs}
+
+$EMACS -batch -Q -l generate-test-data.el
+git clone . issue-124-testbench || true
 
 for branch in master scratch/fix-124-mkcms scratch/fix-124-joaotavora; do
-    git checkout $branch >/dev/null 2>&1
-    (make clean && make eglot.elc) >/dev/null 2>&1
-    echo $branch `emacs -batch -Q -l eglot.elc -l run-benchmark.el`s
+    (cd issue-124-testbench && git checkout -f $branch && make clean && make eglot.elc) 1>&2
+    echo $branch `$EMACS -batch -Q --eval '(package-initialize)' -l issue-124-testbench/eglot.elc -l run-benchmark.el`s
 done
