joaotavora/eglot

commit 212a7f7ba5f8480b3f94c41e77e21e953685937c

Per #315: call shutdown/exit methods with params:{}, not null
"null" is not a valid JSON value for "params" according to the
JSON-RPC specification.

* eglot.el (eglot-shutdown): Do the same thing as for "initialized",
and use an empty hash table to be serialized to {}.

Copyright-paperwork-exempt: yes

eglot.el
@@ -595,8 +595,9 @@ SERVER.  ."
   (unwind-protect
       (progn
         (setf (eglot--shutdown-requested server) t)
-        (jsonrpc-request server :shutdown nil :timeout (or timeout 1.5))
-        (jsonrpc-notify server :exit nil))
+        (jsonrpc-request server :shutdown (make-hash-table)
+                         :timeout (or timeout 1.5))
+        (jsonrpc-notify server :exit (make-hash-table)))
     ;; Now ask jsonrpc.el to shut down the server.
     (jsonrpc-shutdown server (not preserve-buffers))
     (unless preserve-buffers (kill-buffer (jsonrpc-events-buffer server)))))

commit 4f6e152e1c5efc39a888f747ecca313a58f4375e

Fix #430: send shutdown and exit messages without arguments
Fix regression introduced in 212a7f7b (#315).  According to the
LSP specification the exit notification and the shutdown request
shouldn't have arguments ("params: void").  Note that jsonrpc.el
send nil as null on the wire.

* eglot.el (eglot-shutdown): Change back the arguments of
:shutdown and :exit to nil.

eglot.el
@@ -598,9 +598,8 @@ SERVER.  ."
   (unwind-protect
       (progn
         (setf (eglot--shutdown-requested server) t)
-        (jsonrpc-request server :shutdown eglot--{}
-                         :timeout (or timeout 1.5))
-        (jsonrpc-notify server :exit eglot--{}))
+        (jsonrpc-request server :shutdown nil :timeout (or timeout 1.5))
+        (jsonrpc-notify server :exit nil))
     ;; Now ask jsonrpc.el to shut down the server.
     (jsonrpc-shutdown server (not preserve-buffers))
     (unless preserve-buffers (kill-buffer (jsonrpc-events-buffer server)))))