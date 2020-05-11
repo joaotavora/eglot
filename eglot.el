fb82c10eec2b68d33968e703d751e8e3218fc96d
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2020-05-11 15:16:53 -0400

    reported (project) root is relative to server process

--- eglot.el
+++ eglot.el
@@ -914,7 +914,8 @@ This docstring appeases checkdoc, that's all."
                       (list :processId (unless (eq (jsonrpc-process-type server)
                                                    'network)
                                          (emacs-pid))
-                            :rootPath (expand-file-name default-directory)
+                            :rootPath (file-local-name (expand-file-name
+                                                         default-directory))
                             :rootUri (eglot--path-to-uri default-directory)
                             :initializationOptions (eglot-initialization-options
                                                     server)
