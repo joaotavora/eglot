33f084a11e8616dabe5124e64453e2482bf0f99f
Brian Leung <bkleung89@gmail.com>
2021-02-08 18:10:53 -0800

    eglot-client-capabilities: Indicate support for activeParameter
    * eglot.el (eglot-client-capabilities): Mention
    :activeParameterSupport.
    
    Fixup of commit 1f2b0241ca24d78edbd146d17daef6a0e6b2e4f3.

--- eglot.el
+++ eglot.el
@@ -565,7 +565,8 @@ treated as in `eglot-dbind'."
              :signatureHelp      (list :dynamicRegistration :json-false
                                        :signatureInformation
                                        `(:parameterInformation
-                                         (:labelOffsetSupport t)))
+                                         (:labelOffsetSupport t)
+                                         :activeParameterSupport t))
              :references         `(:dynamicRegistration :json-false)
              :definition         `(:dynamicRegistration :json-false)
              :declaration        `(:dynamicRegistration :json-false)
