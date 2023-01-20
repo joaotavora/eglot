368fd4124faa1079c832a8c3f94db1bad5cde720
ꙮ <ymir@ulthar.xyz>
2023-01-20 09:14:10 -0800

    Use the same server command for cperl mode as perl mode

--- eglot.el
+++ eglot.el
@@ -233,6 +233,7 @@ language-server/bin/php-language-server.php"))
                                 (csharp-mode . ("omnisharp" "-lsp"))
                                 (purescript-mode . ("purescript-language-server" "--stdio"))
                                 (perl-mode . ("perl" "-MPerl::LanguageServer" "-e" "Perl::LanguageServer::run"))
+                                (cperl-mode . ("perl" "-MPerl::LanguageServer" "-e" "Perl::LanguageServer::run"))
                                 (markdown-mode . ("marksman" "server")))
   "How the command `eglot' guesses the server to start.
 An association list of (MAJOR-MODE . CONTACT) pairs.  MAJOR-MODE
